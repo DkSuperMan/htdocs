fa1027e1254da6f4f9082ad562d8f2cf9fc298d9
chenjin0624 <158555144@qq.com>
2015-12-02 13:16:46 +0800

    修改请求发送失败的问题
    修改请求发送失败的问题

--- startTask.php
+++ startTask.php
@@ -9,9 +9,7 @@
 
 	function getTask()
 	{
-		// http://zhuan.9766.com/index/neice.php?c=weizhuan&m=api&a=articles_ios cid=2
-
-		$post_data = array ('token' => '754971d1eec66bb38176d8c4cedbacd9','uid' => '16772838','cid' =>'2' ,'page_end' => '10','page_sta' => '1');
+		$post_data = array ('token' => '754971d1eec66bb38176d8c4cedbacd9','uid' => '16772838','page_end' => '10','page_sta' => '1');
 
 		$user_agent = "Mozilla/5.0 (iPhone; iOS 8.3; Scale/2.00)";
 
@@ -20,10 +18,7 @@
 
 		$jsonContent = curl_string($url_page,$user_agent,$post_data,"223.104.4.121");
 
-		print_r($jsonContent);
-
-		$content = json_decode($jsonContent);
-
+		$content = json_decode($jsonContent,true);
 
 		print_r($content);
 	}
@@ -37,37 +32,33 @@
 	function curl_string ($url,$user_agent,$post_data,$cheatip){
 
 		$ch = curl_init();
-
-
-		print_r($post_data);
-
-		print_r($url);
-
-		print_r($user_agent);
-
 		print_r($cheatip);
 
 		echo "<br>";
 
-		// print_r(array('Content-Type' => 'application/x-www-form-urlencoded', "CLIENT-IP:$cheatip", "X-FORWARDED-FOR:$cheatip"));
-		echo "<br>";
-		// echo  "json_encode($post_data)";
-		// return;
 		curl_setopt ($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data)); 
-		// curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data)); 
+
+		// 抓包 start
+		curl_setopt($ch, CURLOPT_PROXY, "127.0.0.1");
+		curl_setopt($ch, CURLOPT_PROXYPORT, 8888);
+		// 抓包 end
+
 		curl_setopt ($ch, CURLOPT_USERAGENT, $user_agent);
-		$cookie_file = tempnam('./temp','cookie'); 
-		curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file);
-		curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_file);
-		curl_setopt ($ch, CURLOPT_HEADER, 1);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		// curl_setopt ($ch, CURLOPT_HTTPHEADER, array('Content-Type' => 'application/x-www-form-urlencoded', "CLIENT-IP:$cheatip", "X-FORWARDED-FOR:$cheatip"));  //此处可以改为任意假IP
+		// $cookie_file = tempnam('./temp','cookie'); 
+		// curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file);
+		// curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_file);
+		// curl_setopt ($ch, CURLOPT_HEADER, 1);
+		curl_setopt ($ch, CURLOPT_POST, 1);
+		curl_setopt ($ch, CURLOPT_HTTPHEADER, array('Content-Type' => 'application/json; charset=utf-8', "CLIENT-IP:$cheatip", "X-FORWARDED-FOR:$cheatip"));  //此处可以改为任意假IP
 		curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, 1);
 		curl_setopt ($ch, CURLOPT_TIMEOUT, 30);
 
-		$result = curl_exec ($ch);
+		curl_exec ($ch);
+
+		$result = curl_multi_getcontent($ch);
+
 		curl_close($ch);
 		return $result;
 	}
@@ -76,10 +67,10 @@
 	// $user_agent = "Mozilla/5.0 (iPhone; iOS 8.3; Scale/2.00)";
 	// // $proxy = "http://115.148.25.187:9000";    //此处为代理服务器IP和PORT高级匿名代理
 	// $string = curl_string($url_page,$user_agent,$proxy);
-
 	// echo $string;
 
-	// echo "<br>";
+
+	echo "<br>";
 	echo "结束任务";
 	echo "<br>";
 
